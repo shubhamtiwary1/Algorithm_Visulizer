51ae06e3b86c8e874ba3ca8fcb2ebc294684ea34
shubhamtiwary1 <tiwaryshubham99@gmail.com>
2024-12-04 12:36:57 +0530

    Initial commit

---
+++ src/main/java/com/example/avisuals/AvisualsApplication.java
@@ -0,0 +1,13 @@
+package com.example.avisuals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisualsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisualsApplication.class, args);
+	}
+
+}
